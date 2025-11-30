2ce35623d602c51216689833299a5e314924a9e2
Elnix90 <elnix91@gmail.com>
2025-11-30 10:29:38 +0100

    fixed auto separate that was teleporting aroud the top

--- app/src/main/java/org/elnix/dragonlauncher/utils/circles/absAngleDiff.kt
+++
@@ -1,9 +0,0 @@
-package org.elnix.dragonlauncher.utils.circles
-
-import kotlin.math.abs
-import kotlin.math.min
-
-fun absAngleDiff(a: Double, b: Double): Double {
-    val diff = abs(a - b)
-    return min(diff, 360 - diff)
-}
--- app/src/main/java/org/elnix/dragonlauncher/utils/circles/autoSeparate.kt
+++ app/src/main/java/org/elnix/dragonlauncher/utils/circles/autoSeparate.kt
@@ -3,6 +3,8 @@ package org.elnix.dragonlauncher.utils.circles
 import org.elnix.dragonlauncher.data.UiSwipePoint
 import org.elnix.dragonlauncher.ui.MIN_ANGLE_GAP
 import kotlin.collections.filter
+import kotlin.math.abs
+import kotlin.math.min
 
 fun autoSeparate(points: MutableList<UiSwipePoint>, circleNumber: Int) {
     val pts = points.filter { it.circleNumber == circleNumber }
@@ -13,15 +15,26 @@ fun autoSeparate(points: MutableList<UiSwipePoint>, circleNumber: Int) {
 
         for (i in pts.indices) {
             for (j in i + 1 until pts.size) {
-
                 val p1 = pts[i]
                 val p2 = pts[j]
-                val diff = absAngleDiff(p1.angleDeg, p2.angleDeg)
 
+                val a = normalizeAngle(p1.angleDeg)
+                val b = normalizeAngle(p2.angleDeg)
+
+                val diff = absAngleDiff(a, b)
                 if (diff < MIN_ANGLE_GAP) {
-                    val mid = (p1.angleDeg + p2.angleDeg) / 2
-                    p1.angleDeg = (mid - MIN_ANGLE_GAP / 2 + 360) % 360
-                    p2.angleDeg = (mid + MIN_ANGLE_GAP / 2 + 360) % 360
+                    // signed shortest difference from a -> b in range (-180, 180]
+                    val signed = signedAngleDiff(a, b) // b - a in signed form
+                    // midpoint along the shortest arc
+                    val mid = normalizeAngle(a + signed / 2.0)
+
+                    // place the two points symmetrically around mid
+                    val halfGap = MIN_ANGLE_GAP / 2.0
+                    val newA = normalizeAngle(mid - halfGap)
+                    val newB = normalizeAngle(mid + halfGap)
+
+                    p1.angleDeg = newA
+                    p2.angleDeg = newB
                     adjusted = true
                 }
             }
@@ -29,4 +42,31 @@ fun autoSeparate(points: MutableList<UiSwipePoint>, circleNumber: Int) {
 
         if (!adjusted) return
     }
+}
+
+/** Normalize angle into [0,360) */
+private fun normalizeAngle(a: Double): Double {
+    val v = a % 360.0
+    return if (v < 0) v + 360.0 else v
+}
+
+/**
+ * Return absolute minimal difference between two angles (0..180)
+ */
+fun absAngleDiff(a: Double, b: Double): Double {
+    val diff = abs(a - b)
+    return min(diff, 360 - diff)
+}
+
+/**
+ * Signed shortest difference from a -> b in degrees, in range (-180, 180]
+ * Example: a=350, b=10 -> returns +20
+ *          a=10,  b=350 -> returns -20
+ */
+private fun signedAngleDiff(a: Double, b: Double): Double {
+    // compute raw difference
+    var d = (b - a) % 360.0
+    if (d <= -180.0) d += 360.0
+    else if (d > 180.0) d -= 360.0
+    return d
 }
