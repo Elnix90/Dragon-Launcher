05b273cb0fbf154529a35a92cadded396d703bbe
Elnix90 <elnix91@gmail.com>
2025-12-03 20:42:50 +0100

    Updated to 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "org.elnix.dragonlauncher"
         minSdk = 27
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "1.1.1"
     }
 
 
