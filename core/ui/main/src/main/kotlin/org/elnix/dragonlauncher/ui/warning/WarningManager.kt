0a2bd92c007bf24bc5a3310c923e567bdd30f0cf
Elnix90 <elnix91@proton.me>
2026-07-24 11:50:27 +0200

    fix: updated date of google lockdown to the new date they announced

--- core/ui/main/src/main/kotlin/org/elnix/dragonlauncher/ui/warning/WarningManager.kt
+++ core/ui/main/src/main/kotlin/org/elnix/dragonlauncher/ui/warning/WarningManager.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2026  Shubham Gorai
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
 package org.elnix.dragonlauncher.ui.warning
 
 import kotlinx.datetime.LocalDate
@@ -39,6 +23,6 @@ public object WarningManager {
         return Clock.System.now()
             .toLocalDateTime(TimeZone.currentSystemDefault())
             .date
-            .daysUntil(LocalDate(year = 2026, month = 9, day = 1))
+            .daysUntil(LocalDate(year = 2027, month = 1, day = 1))
     }
 }
