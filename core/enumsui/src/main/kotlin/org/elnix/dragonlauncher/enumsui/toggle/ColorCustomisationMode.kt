718813514c00ca5ec6e142d0efb2f085315aaf4e
Elnix90 <elnix91@proton.me>
2026-07-24 13:07:26 +0200

    fix: renamed light theme into flashbang theme cause it deserve it

--- core/enumsui/src/main/kotlin/org/elnix/dragonlauncher/enumsui/toggle/ColorCustomisationMode.kt
+++ core/enumsui/src/main/kotlin/org/elnix/dragonlauncher/enumsui/toggle/ColorCustomisationMode.kt
@@ -9,7 +9,7 @@ public enum class DefaultThemes(
     override val iconEnabled: Int? = null,
     override val iconDisabled: Int? = null
 ) : ToggleButtonOption {
-    Light(R.string.light_theme),
+    Light(R.string.flashbang_theme),
     Dark(R.string.dark_theme),
     Amoled(R.string.amoled_theme),
     System(R.string.system_theme),
