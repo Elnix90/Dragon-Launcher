30c2f7fe6f9fbbf56cea7c00fc5aff3842012b75
Elnix90 <elnix91@proton.me>
2026-07-24 10:29:10 +0200

    fix: removed useless LocalContentColor in BetaVersionWarning.kt

--- core/ui/main/src/main/kotlin/org/elnix/dragonlauncher/ui/components/BetaVersionWarning.kt
+++ core/ui/main/src/main/kotlin/org/elnix/dragonlauncher/ui/components/BetaVersionWarning.kt
@@ -11,13 +11,11 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialShapes
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.toShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -46,77 +44,72 @@ public fun BetaVersionWarning(
     val ctx = LocalContext.current
     val scope = rememberCoroutineScope()
 
-
-    CompositionLocalProvider(
-        LocalContentColor provides MaterialTheme.colorScheme.onErrorContainer
+    Card(
+        shape = MaterialTheme.shapes.extraLarge,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.errorContainer,
+            contentColor = MaterialTheme.colorScheme.onErrorContainer
+        )
     ) {
-        Card(
-            shape = MaterialTheme.shapes.extraLarge,
-            colors = CardDefaults.cardColors(
-                containerColor = MaterialTheme.colorScheme.errorContainer,
-                contentColor = MaterialTheme.colorScheme.onErrorContainer
-            )
+        Column(
+            modifier = Modifier.padding(12.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Column(
-                modifier = Modifier.padding(12.dp),
-                verticalArrangement = Arrangement.spacedBy(10.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(12.dp),
+                verticalAlignment = Alignment.CenterVertically
             ) {
-
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(12.dp),
-                    verticalAlignment = Alignment.CenterVertically
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .size(48.dp)
+                        .background(
+                            color = MaterialTheme.colorScheme.error,
+                            shape = MaterialShapes.Arrow.toShape()
+                        )
                 ) {
-                    Box(
-                        contentAlignment = Alignment.Center,
-                        modifier = Modifier
-                            .size(48.dp)
-                            .background(
-                                color = MaterialTheme.colorScheme.error,
-                                shape = MaterialShapes.Arrow.toShape()
-                            )
-                    ) {
-                        Icon(
-                            painter = painterResource(R.drawable.warning),
-                            contentDescription = stringResource(R.string.warning)
-                        )
-                    }
-
-                    Text(
-                        text = stringResource(R.string.warning),
-                        style = MaterialTheme.typography.titleMediumEmphasized
+                    Icon(
+                        painter = painterResource(R.drawable.warning),
+                        contentDescription = stringResource(R.string.warning)
                     )
-
-                    Spacer()
-
-                    if (betaVersionType == BetaVersionType.App) {
-                        DragonIconButton(
-                            onClick = {
-                                scope.launch {
-                                    PrivateSettingsStore.hideBetaVersionWarning.set(ctx, true)
-                                }
-                            },
-                            icon = R.drawable.close,
-                            contentDescription = stringResource(R.string.close),
-                            colors = AppObjectsColors.cancelIconButtonColors()
-                        )
-                    }
                 }
 
-
-                val warningText = stringResource(
-                    when (betaVersionType) {
-                        BetaVersionType.App -> R.string.this_is_a_beta_version
-                        BetaVersionType.Feature -> R.string.this_feature_is_in_beta
-                    }
-                )
-
                 Text(
-                    text = warningText,
-                    style = MaterialTheme.typography.labelSmall,
-                    fontSize = 12.sp
+                    text = stringResource(R.string.warning),
+                    style = MaterialTheme.typography.titleMediumEmphasized
                 )
+
+                Spacer()
+
+                if (betaVersionType == BetaVersionType.App) {
+                    DragonIconButton(
+                        onClick = {
+                            scope.launch {
+                                PrivateSettingsStore.hideBetaVersionWarning.set(ctx, true)
+                            }
+                        },
+                        icon = R.drawable.close,
+                        contentDescription = stringResource(R.string.close),
+                        colors = AppObjectsColors.cancelIconButtonColors()
+                    )
+                }
             }
+
+
+            val warningText = stringResource(
+                when (betaVersionType) {
+                    BetaVersionType.App -> R.string.this_is_a_beta_version
+                    BetaVersionType.Feature -> R.string.this_feature_is_in_beta
+                }
+            )
+
+            Text(
+                text = warningText,
+                style = MaterialTheme.typography.labelSmall,
+                fontSize = 12.sp
+            )
         }
     }
 }
