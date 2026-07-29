2139c51459ad18a6d8064111080a45382629a6b3
Elnix90 <elnix91@proton.me>
2026-07-29 22:38:58 +0200

    fix: corrected closeApp function to also exitProcess

--- core/common/src/main/kotlin/org/elnix/dragonlauncher/common/utils/LifecycleUtils.kt
+++ core/common/src/main/kotlin/org/elnix/dragonlauncher/common/utils/LifecycleUtils.kt
@@ -1,9 +1,11 @@
 package org.elnix.dragonlauncher.common.utils
 
 import androidx.activity.ComponentActivity
+import kotlin.system.exitProcess
 
 public object LifecycleUtils {
     public fun closeApp(activity: ComponentActivity) {
         activity.finishAffinity()
+        exitProcess(0)
     }
 }
