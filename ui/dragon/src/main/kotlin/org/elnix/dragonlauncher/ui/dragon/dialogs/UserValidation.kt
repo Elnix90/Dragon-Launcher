8c5675aa7af61054a481227b49082454eae3c121
Elnix90 <elnix91@proton.me>
2026-07-31 14:28:09 +0200

    fix: corrected UserValidation and added useless micro interaction cuz I felt it

--- ui/dragon/src/main/kotlin/org/elnix/dragonlauncher/ui/dragon/dialogs/UserValidation.kt
+++ ui/dragon/src/main/kotlin/org/elnix/dragonlauncher/ui/dragon/dialogs/UserValidation.kt
@@ -6,17 +6,18 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialShapes
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -25,7 +26,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
@@ -33,12 +33,27 @@ import org.elnix.dragonlauncher.common.utils.CopyPasteUtils.copyToClipboard
 import org.elnix.dragonlauncher.i18n.R
 import org.elnix.dragonlauncher.material.shapes.toShape
 import org.elnix.dragonlauncher.theme.AppObjectsColors
+import org.elnix.dragonlauncher.ui.base.animation.Icon
+import org.elnix.dragonlauncher.ui.base.animation.rememberAnimatedIcon
 import org.elnix.dragonlauncher.ui.base.components.Spacer
 import org.elnix.dragonlauncher.ui.dragon.components.DragonIconButton
 import org.elnix.dragonlauncher.ui.dragon.components.ValidateCancelButtons
 import org.elnix.dragonlauncher.ui.dragon.text.DialogDescription
 import org.elnix.dragonlauncher.ui.dragon.text.DialogTitle
 
+
+private val useLessicons = setOf(
+    R.drawable.shapes,
+    R.drawable.casino,
+    R.drawable.shape_line,
+    R.drawable.visibility,
+    R.drawable.account_circle,
+    R.drawable.android,
+    R.drawable.alternate_email,
+    R.drawable.crop_free,
+    R.drawable.discord_symbol_blurple,
+    R.drawable.check
+)
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
@@ -58,6 +73,8 @@ fun UserValidation(
 ) {
     val ctx = LocalContext.current
     var doNotRemindMeAgainChecked by remember { mutableStateOf(false) }
+
+    val uselessAnimation = rememberAnimatedIcon()
 
     AlertDialog(
         onDismissRequest = onDismiss ?: onValidate,
@@ -80,27 +97,31 @@ fun UserValidation(
                         shape = MaterialShapes.Pill.toShape()
                     )
             ) {
-                Icon(
-                    painter = painterResource(titleIcon),
-                    contentDescription = null,
-                    tint = titleColor
-                )
+                var recompose by remember { mutableIntStateOf(0) }
+                val successIcon = remember(recompose) { useLessicons.random() }
+                uselessAnimation.Icon(
+                    defaultIcon = titleIcon,
+                    successIcon = successIcon,
+                    defaultColor = titleColor,
+                    successColor = titleColor,
+                    modifier = Modifier.size(32.dp)
+                ) {
+                    recompose++
+                    uselessAnimation.setSuccess()
+                }
             }
         },
         title = {
             if (title != null) {
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(12.dp),
-                    verticalAlignment = Alignment.CenterVertically,
+                DialogTitle(
+                    text = title,
                     modifier = Modifier
+                        .fillMaxWidth()
                         .padding(bottom = 4.dp)
                         .clip(MaterialTheme.shapes.large)
                         .background(MaterialTheme.colorScheme.surface)
                         .padding(8.dp)
-
-                ) {
-                    DialogTitle(title)
-                }
+                )
             }
         },
         text = {
