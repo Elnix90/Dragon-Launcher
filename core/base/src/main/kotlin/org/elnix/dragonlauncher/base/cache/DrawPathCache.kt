46acdcf930a2305f1802b0ac6141d42921082a8c
Elnix90 <elnix91@proton.me>
2026-08-03 11:31:33 +0200

    doc: added doc

--- core/base/src/main/kotlin/org/elnix/dragonlauncher/base/cache/DrawPathCache.kt
+++ core/base/src/main/kotlin/org/elnix/dragonlauncher/base/cache/DrawPathCache.kt
@@ -6,4 +6,10 @@ import androidx.compose.ui.graphics.Path
 import org.elnix.dragonlauncher.base.DragonCache
 import org.elnix.dragonlauncher.base.model.serializables.IconShape
 
+/**
+ * Handles a cache where a icon shape and a size outputs a path, to avoid computing them everytime.
+ *
+ * I really don't want to handle the cache invalidation of this thing, so I'll keep it at 200 and call it a day.
+ * Usually ppl don't usually have more than 200 points with sizes and shapes different, or they might be psychopaths
+ */
 public object DrawPathCache : DragonCache<Pair<IconShape, Size>, Path>(200)
