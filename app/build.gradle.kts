0ca70ef19b5146c8d55ae8aa19bac5413a94e20c
Elnix90 <elnix91@proton.me>
2026-08-21 12:05:41 +0200

    bump: version 4.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ extensions.configure<ApplicationExtension> {
 
     defaultConfig {
         applicationId = "org.elnix.dragonlauncher"
-        versionName = "4.0.1"
-        versionCode = 58
+        versionName = "4.0.2"
+        versionCode = 59
     }
 
     val hasSigningConfig = env("KEYSTORE_FILE") != null &&
