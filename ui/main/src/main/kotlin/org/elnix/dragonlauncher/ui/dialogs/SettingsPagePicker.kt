886c7bf6ffc2e05655067dbbd13de60aee4aff2f
Elnix90 <elnix91@proton.me>
2026-08-29 11:33:46 +0200

    fix(ui): improved SettingsPagePicker ui with a modal bottom sheet

--- ui/main/src/main/kotlin/org/elnix/dragonlauncher/ui/dialogs/SettingsPagePicker.kt
+++ ui/main/src/main/kotlin/org/elnix/dragonlauncher/ui/dialogs/SettingsPagePicker.kt
@@ -1,69 +1,63 @@
 package org.elnix.dragonlauncher.ui.dialogs
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import org.elnix.dragonlauncher.base.navigation.NavigationRoute
 import org.elnix.dragonlauncher.base.navigation.NavigationRoute.Companion.settingsRoutes
 import org.elnix.dragonlauncher.i18n.R
 import org.elnix.dragonlauncher.ui.base.components.LazyColumnWithScrollIndicator
-import org.elnix.dragonlauncher.ui.dragon.dialogs.CustomAlertDialog
+import org.elnix.dragonlauncher.ui.base.components.Spacer
+import org.elnix.dragonlauncher.ui.dragon.components.DragonModalBottomSheet
+import org.elnix.dragonlauncher.ui.dragon.components.DragonSettingsGroup
+import org.elnix.dragonlauncher.ui.dragon.text.DialogTitle
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingsPagePicker(
     onDismissRequest: () -> Unit,
     onSelect: (NavigationRoute) -> Unit
 ) {
-    CustomAlertDialog(
-        modifier = Modifier.padding(40.dp),
-        onDismissRequest = onDismissRequest,
-        alignment = Alignment.Center,
-        scroll = false,
-        title = {
-            Text(
-                text = stringResource(R.string.pick_a_settings_screen),
-                color = MaterialTheme.colorScheme.onSurface,
-                style = MaterialTheme.typography.titleMedium
-            )
-        },
-        text = {
+    DragonModalBottomSheet(onDismissRequest, true) {
+        DialogTitle(stringResource(R.string.pick_a_settings_screen))
+        Spacer(5.dp)
+
+        DragonSettingsGroup {
             LazyColumnWithScrollIndicator(
                 items = settingsRoutes,
-                modifier = Modifier.heightIn(max = 500.dp)
+                modifier = Modifier.heightIn(max = 600.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
             ) { route ->
-                Card(
-                    onClick = { onSelect(route) }
-                ) {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 10.dp, horizontal = 5.dp),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.spacedBy(5.dp)
-                    ) {
-                        RadioButton(
-                            selected = false,
-                            onClick = null
-                        )
 
-                        Text(
-                            text = stringResource(route.resId),
-                            style = MaterialTheme.typography.labelMedium
-                        )
-                    }
+                Row(
+                    modifier = Modifier.dragonSettingGroup {
+                        clickable { onSelect(route) }
+                    },
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(route.icon),
+                        contentDescription = null
+                    )
+
+                    Text(
+                        text = stringResource(route.resId),
+                        style = MaterialTheme.typography.labelMedium
+                    )
                 }
             }
         }
-    )
+    }
 }
