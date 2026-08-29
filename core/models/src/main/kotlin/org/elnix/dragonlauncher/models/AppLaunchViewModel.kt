b9b49eeb359f70af9c0a42564502dd3a1f1da18d
Elnix90 <elnix91@proton.me>
2026-08-29 16:26:07 +0200

    fix: remove logWtf from release version

--- core/models/src/main/kotlin/org/elnix/dragonlauncher/models/AppLaunchViewModel.kt
+++ core/models/src/main/kotlin/org/elnix/dragonlauncher/models/AppLaunchViewModel.kt
@@ -10,7 +10,6 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.elnix90.logging.logE
 import io.github.elnix90.logging.logI
 import io.github.elnix90.logging.logW
-import io.github.elnix90.logging.logWtf
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -72,11 +71,8 @@ public class AppLaunchViewModel @Inject constructor(
 
             val app = appRepository.findOne(launchAction).first()
             if (app != null) {
-                logWtf(APP_LAUNCH_TAG) { "Found app corresponding: ${app.packageName}, ${app.profile}, Launching it!" }
                 requestAppLaunch(app)
             } else {
-                logWtf(APP_LAUNCH_TAG) { "No app found for: $launchAction" }
-
                 launchLockedApp(profile, launchAction)
             }
         }
