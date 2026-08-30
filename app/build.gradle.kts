693e63fa7896fdf51dde8b36d39d8608e1fe74e0
Elnix90 <elnix91@proton.me>
2026-08-30 15:13:39 +0200

    chore: update 4.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ extensions.configure<ApplicationExtension> {
 
     defaultConfig {
         applicationId = "org.elnix.dragonlauncher"
-        versionName = "4.1.1"
-        versionCode = 61
+        versionName = "4.1.2"
+        versionCode = 62
     }
 
     val hasSigningConfig = env("KEYSTORE_FILE") != null &&
