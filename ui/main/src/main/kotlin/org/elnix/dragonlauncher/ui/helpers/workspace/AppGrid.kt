e6155845da6eea2a1b98bc322e89885faea90d04
Elnix90 <elnix91@proton.me>
2026-08-31 17:43:01 +0200

    fix(crash): accessing app in apps array even when array is empty
    fix(crash): onLongClick argument that should have been null
    
    fix: category itself is clickable and clipped by MaterialTheme.shapes

--- ui/main/src/main/kotlin/org/elnix/dragonlauncher/ui/helpers/workspace/AppGrid.kt
+++ ui/main/src/main/kotlin/org/elnix/dragonlauncher/ui/helpers/workspace/AppGrid.kt
@@ -3,6 +3,7 @@ package org.elnix.dragonlauncher.ui.helpers.workspace
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -36,6 +37,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -47,7 +49,6 @@ import org.elnix.dragonlauncher.base.model.models.AppCategory
 import org.elnix.dragonlauncher.base.model.models.Application
 import org.elnix.dragonlauncher.i18n.R
 import org.elnix.dragonlauncher.settings.stores.map.DrawerSettingsStore
-import org.elnix.dragonlauncher.ui.base.modifiers.shapedClickable
 import org.elnix.dragonlauncher.ui.compositionslocals.LocalDrawerSettings
 import org.elnix.dragonlauncher.ui.dragon.components.DragonIconButton
 import org.elnix.dragonlauncher.ui.drawer.AppItemGrid
@@ -343,12 +344,12 @@ private fun CategoryGrid(
             modifier =
                 modifier
                     .aspectRatio(1f)
-                    .shapedClickable { onOpenCategory() }
                     .padding(10.dp)
         ) {
             AppDefinedGrid(
                 apps = apps,
                 longPressPopup = longPressPopup,
+                onOpenCategory = onOpenCategory,
                 onClick = onClick,
                 gridCells = categoryGridCells
             )
@@ -370,6 +371,7 @@ private fun AppDefinedGrid(
     gridCells: Int,
     modifier: Modifier = Modifier,
     onLongClick: ((Application) -> Unit)? = null,
+    onOpenCategory: () -> Unit,
     longPressPopup: Boolean,
     onClick: ((Application) -> Unit)?
 ) {
@@ -383,6 +385,8 @@ private fun AppDefinedGrid(
         modifier =
             modifier
                 .fillMaxSize()
+                .clip(MaterialTheme.shapes.medium)
+                .clickable(onClick = onOpenCategory)
                 .background(MaterialTheme.colorScheme.surfaceVariant)
     ) {
         repeat(gridCells) {
@@ -394,13 +398,18 @@ private fun AppDefinedGrid(
                         modifier = Modifier.weight(1f),
                         contentAlignment = Alignment.Center
                     ) {
-                        val app = apps[appIndex]
-
                         if (appIndex < sanitizedAppNumber) {
+                            val app = apps[appIndex]
+
                             AppItemGrid(
                                 app = app,
                                 selected = false,
-                                onLongClick = { onLongClick?.invoke(app) },
+                                onLongClick =
+                                    if (onLongClick != null) {
+                                        { onLongClick(app) }
+                                    } else {
+                                        null
+                                    },
                                 longPressPopup = longPressPopup,
                                 onClick = { onClick?.invoke(app) }
                             )
