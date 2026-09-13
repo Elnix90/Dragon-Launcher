5a62c2f15888e4ffd391acfc7454c87689894c14
Elnix90 <elnix91@proton.me>
2026-09-13 15:49:25 +0200

    chore: bump version to  4.2.0 - Nounou

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ extensions.configure<ApplicationExtension> {
 
     defaultConfig {
         applicationId = "org.elnix.dragonlauncher"
-        versionName = "4.1.4"
-        versionCode = 64
+        versionName = "4.2.0"
+        versionCode = 65
     }
 
     val hasSigningConfig =
